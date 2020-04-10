a7d3aa3e5466c63ba309892135c69a1304f45955
Tyler Jaacks <TylerJaacks@Gmail.com>
2020-04-10 16:17:55 -0500

    Added filename parameter for hd5processor.

--- src/edu/iastate/tofmsdataanalysis/hdf5processor/hdf5processor.py
+++ src/edu/iastate/tofmsdataanalysis/hdf5processor/hdf5processor.py
@@ -1,55 +1,65 @@
-import string
+import sys
 
 import h5py
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-hf = h5py.File('D:\\Test.h5', 'r')
 
-n1 = np.array(hf["PeakData"]["PeakData"])
-raw_peak_data = np.array(hf["PeakData"]["PeakData"])
+def hdf5processor(filename):
+    hf = h5py.File(filename, 'r')
 
-reshaped_peak_data = np.reshape(raw_peak_data, (100000, 286))
+    n1 = np.array(hf["PeakData"]["PeakData"])
+    raw_peak_data = np.array(hf["PeakData"]["PeakData"])
 
-number_of_waveforms = int(hf.attrs['NbrWaveforms'])
+    reshaped_peak_data = np.reshape(raw_peak_data, (100000, 286))
 
-reshaped_peak_data_in_counts = reshaped_peak_data * number_of_waveforms
+    number_of_waveforms = int(hf.attrs['NbrWaveforms'])
 
-isotopes = np.array(hf['PeakData']['PeakTable']['label'])
-isotopes_new = [None] * isotopes.size
+    reshaped_peak_data_in_counts = reshaped_peak_data * number_of_waveforms
 
-for i in range(isotopes.size):
-    isotopes_new[i] = str(isotopes[i]).replace('b', '').replace('[', '').replace(']', '').replace('+', '').replace('\'',
-                                                                                                                   '')
+    isotopes = np.array(hf['PeakData']['PeakTable']['label'])
+    isotopes_new = [None] * isotopes.size
 
-element_isotope_map = {
-    'Ti': ['47Ti', '49Ti'],
-    'Cr': ['52Cr', '53Cr'],
-    'Fe': ['54Fe', '57Fe'],
-    'Mn': ['55Mn']}
+    for i in range(isotopes.size):
+        isotopes_new[i] = str(isotopes[i]).replace('b', '').replace('[', '').replace(']', '').replace('+', '').replace(
+            '\'',
+            '')
 
-n2 = np.reshape(n1, (100000, 286))
-element_summed_data_map = {}
+    element_isotope_map = {
+        'Ti': ['47Ti', '49Ti'],
+        'Cr': ['52Cr', '53Cr'],
+        'Fe': ['54Fe', '57Fe'],
+        'Mn': ['55Mn']}
 
-print(n2)
-for key, value in element_isotope_map.items():
-    if len(value) == 1:
-        element_summed_data_map.update({key: reshaped_peak_data_in_counts[:, isotopes_new.index(value[0])]})
-    elif len(value) == 2:
-        new_summed_data = reshaped_peak_data_in_counts[:, isotopes_new.index(value[0])] + \
-                          reshaped_peak_data_in_counts[:, isotopes_new.index(value[1])]
+    n2 = np.reshape(n1, (100000, 286))
+    element_summed_data_map = {}
 
-        element_summed_data_map.update({key: new_summed_data})
-    elif len(value) == 3:
-        new_summed_data = reshaped_peak_data_in_counts[:, isotopes_new.index(value[0])] + \
-                          reshaped_peak_data_in_counts[:, isotopes_new.index(value[1])] + \
-                          reshaped_peak_data_in_counts[:, isotopes_new.index(value[2])]
+    print(n2)
+    for key, value in element_isotope_map.items():
+        if len(value) == 1:
+            element_summed_data_map.update({key: reshaped_peak_data_in_counts[:, isotopes_new.index(value[0])]})
+        elif len(value) == 2:
+            new_summed_data = reshaped_peak_data_in_counts[:, isotopes_new.index(value[0])] + \
+                              reshaped_peak_data_in_counts[:, isotopes_new.index(value[1])]
 
-        element_summed_data_map.update({key: new_summed_data})
+            element_summed_data_map.update({key: new_summed_data})
+        elif len(value) == 3:
+            new_summed_data = reshaped_peak_data_in_counts[:, isotopes_new.index(value[0])] + \
+                              reshaped_peak_data_in_counts[:, isotopes_new.index(value[1])] + \
+                              reshaped_peak_data_in_counts[:, isotopes_new.index(value[2])]
 
-plt.plot(element_summed_data_map.get('Ti'), label="Ti")
-plt.plot(element_summed_data_map.get('Cr'), label="Cr")
-plt.plot(element_summed_data_map.get('Fe'), label="Fe")
-plt.legend()
-plt.show()
+            element_summed_data_map.update({key: new_summed_data})
 
+    plt.plot(element_summed_data_map.get('Ti'), label="Ti")
+    plt.plot(element_summed_data_map.get('Cr'), label="Cr")
+    plt.plot(element_summed_data_map.get('Fe'), label="Fe")
+    plt.legend()
+    plt.show()
+
+
+def main(argv):
+    hdf5processor(argv[0])
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
